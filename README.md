5f39d8ee23e3494051f17f36052f572bb1a51e00
FebrianAditya <febrian.aditya@student.upi.edu>
2020-12-11 00:38:13 +0700

    1. connection socket IO server and client

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GP2-SERVER
+
+1. Ketika button ditekan dia akan nambahin count dan update di database
+
+2. Kalau udah berfungsi button tersebut diganti dengan suatu gambar yang bisa di klik
+
+3. Home 
--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
 
-let counter = [{
+let counters = [{
     name: "player1",
     counter: 0
 }]
 
 io.on("connect", (socket) => {
-    socket.emit("countClick", counter);
+    // io.in("Game").emit("countClick", counters)
+    socket.broadcast.emit("countClick", counters);
+    // io.emit("countClick", counters);
+
+    socket.on('newCounter', function (payload) {
+      // counters[0].counter = payload.score
+      counters.push(payload.score)
+      console.log(counters.length-1)
+      io.emit('scoreLawan', payload.score)
+    })
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, () => { 
   console.log(`Listening on port ${PORT}`);
 });
