c880f81032aa8f015af1077ed56dbfd2b3d718d0
fajarrrX <fajarrr30@gmail.com>
2020-12-11 02:15:14 +0700

    fixing app.js added some syntax in app.js

--- app.js
+++ app.js
@@ -3,24 +3,32 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
 
-let counters = [{
+let counters = [
+  {
     name: "player1",
-    counter: 0
-}]
+    counter: 0,
+  },
+];
 
 io.on("connect", (socket) => {
-    // io.in("Game").emit("countClick", counters)
-    socket.broadcast.emit("countClick", counters);
-    // io.emit("countClick", counters);
+  // io.in("Game").emit("countClick", counters)
+  io.emit("countClick", counters);
+  // io.emit("countClick", counters);
 
-    socket.on('newCounter', function (payload) {
-      // counters[0].counter = payload.score
-      counters.push(payload.score)
-      console.log(counters.length-1)
-      io.emit('scoreLawan', payload.score)
-    })
+  socket.on("newCounter", function (payload) {
+    // counters[0].counter = payload.score
+    counters.push(payload.score);
+    // console.log(counters.length - 1);
+    socket.broadcast.emit("scoreLawan", payload.score);
+  });
+
+  socket.on("terserahlulah", function (payload) {
+    counters[0].name = payload;
+    socket.emit("username", payload);
+    // console.log(counters[0].name);
+  });
 });
 
-server.listen(PORT, () => { 
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
