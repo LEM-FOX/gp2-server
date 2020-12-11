e7fe350e54fb543ebdcd59db593498669d1f5cb5
ogyrahmawan <ogyrahmawan@gmail.com>
2020-12-11 14:40:37 +0700

    final server

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let counters = [];
 
 let rooms = []
 
+let collectionPlayer = []
+
 io.on("connect", (socket) => {
   // io.in("Game").emit("countClick", counters)
   io.emit("countClick", counters);
@@ -24,25 +26,31 @@ io.on("connect", (socket) => {
     rooms.push(room)
     io.emit('updatedRoom', rooms)
   })
+
   socket.on('joinRoom', payload => {
+    collectionPlayer.push(payload.username)
     socket.join(payload.roomName, () => {
       let roomIndex = rooms.findIndex((i) => i.name == payload.roomName)
       rooms[roomIndex].users.push(payload.username)
-      console.log(rooms[roomIndex], 'ini dari socket join room')
       io.sockets.in(payload.roomName).emit("roomDetail", rooms[roomIndex])
     })
   })
   socket.on('startGame', data => {
-    console.log(data, 'tes')
     io.in(data).emit('startGame')
+    // socket.broadcast.to(data).emit('startGame')
+    io.emit('collectionPlayer', collectionPlayer)
   })
   socket.on("newCounter", function (payload) {
-    counters[0].counter = payload.score
-    
-    socket.broadcast.emit("scoreLawan", payload.score);
+    if(payload.score == 10){
+      io.sockets.in(payload.roomName).emit('gameOver', `${payload.players[0]} win`)
+    } else if(payload.scoreLawan == 10) {
+      io.sockets.in(payload.roomName).emit('gameOver', 'you lose')
+    } else {
+      socket.broadcast.emit("scoreLawan", payload.score);
+    }
+    // console.log(payload)
   });
-
-
+  
 });
 
 server.listen(PORT, () => {
