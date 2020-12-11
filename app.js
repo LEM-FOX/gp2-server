f13ef3de3a2f6c8f3ce69769c921d01436e64b60
ogyrahmawan <ogyrahmawan@gmail.com>
2020-12-11 08:06:39 +0700

    1. menambahkan room 2. merubah object counter 3. merubah socket on terserahlu menjadi player register 4. menambahkan socket room detail

--- app.js
+++ app.js
@@ -3,35 +3,44 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
 
-let counters = [
-  {
-    name: "player1",
-    counter: 0,
-    scoreLawan: 0
-  },
-];
+let counters = [];
+
+let rooms = []
 
 io.on("connect", (socket) => {
   // io.in("Game").emit("countClick", counters)
   io.emit("countClick", counters);
   // io.emit("countClick", counters);
-
+  socket.on("playerRegistration", function (payload) {
+    counters.push(payload)
+    socket.emit("playerData", counters);
+  });
+  socket.on('createRoom', payload => {
+    let room = {
+      name: payload.roomName,
+      users: [],
+      admin : payload.admin
+    }
+    rooms.push(room)
+    console.log(rooms, 'ini dari room')
+    io.emit('updatedRoom', rooms)
+  })
+  socket.on('joinRoom', payload => {
+    socket.join(payload.roomName, () => {
+      let roomIndex = rooms.findIndex((i) => i.name == payload.roomName)
+      rooms[roomIndex].users.push(payload.username)
+      io.sockets.in(payload.roomName).emit("roomDetail", rooms[roomIndex])
+    })
+  })
+  socket.on('startGame', data => {
+    socket.broadcast.to(data).emit('startGame')
+  })
   socket.on("newCounter", function (payload) {
-
-    counters[0].scoreLawan = payload.scoreLawan
     counters[0].counter = payload.score
-
-    // counters[0].counter = payload.score
     socket.broadcast.emit("scoreLawan", payload.score);
-    console.log(counters[0])
-    // console.log(counters.length - 1);
   });
 
-  socket.on("terserahlulah", function (payload) {
-    counters[0].name = payload;
-    socket.emit("username", payload);
-    // console.log(counters[0].name);
-  });
+
 });
 
 server.listen(PORT, () => {
